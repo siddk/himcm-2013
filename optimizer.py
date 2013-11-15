cfc97228f697f97a939afe3760d354ebcb5e7792
Calvin Huang <clhuang@eneron.us>
2013-11-15 13:59:38 -0800

    stuff

--- optimizer.py
+++ optimizer.py
@@ -2,6 +2,8 @@ import numpy as np
 import itertools
 from data import traveltimes, populations
 
+reaches = np.zeros((6, 6))
+
 
 def reach(start, target):
     '''
@@ -9,16 +11,21 @@ def reach(start, target):
     '''
     pass
 
-reaches = np.zeros((6, 6))
-for i in xrange(6):
-    for j in xrange(6):
-        reaches[i, j] = reach(i, j)
 
-maxreach = 0
-optimal = (0, 1, 2)
-for a1, a2, a3 in itertools.combinations(xrange(6), 3):
-    totalreach = 0
+def calcreaches():
+    global reaches
     for i in xrange(6):
-        totalreach += min(populations[i], sum([reaches[a, i] for a in (a1, a2, a3)]))
-    if totalreach > maxreach:
-        optimal = (a1, a2, a3)
+        for j in xrange(6):
+            reaches[i, j] = reach(i, j)
+
+
+def bruteforce():
+    maxreach = 0
+    optimal = (0, 1, 2)
+    for a1, a2, a3 in itertools.combinations(xrange(6), 3):
+        totalreach = 0
+        for i in xrange(6):
+            totalreach += min(populations[i], sum([reaches[a, i] for a in (a1, a2, a3)]))
+        if totalreach > maxreach:
+            optimal = (a1, a2, a3)
+    return optimal
